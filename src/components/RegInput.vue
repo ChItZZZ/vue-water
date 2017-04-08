6041d17d1e81516c9898b50bc96f176c589f3aba
ChItZZZ <648267970@qq.com>
2017-04-09 02:02:03 +0800

    add sass and register page

--- src/components/RegInput.vue
+++ src/components/RegInput.vue
@@ -1,11 +1,39 @@
 <template>
-    <div class="reg-text">
-        <div class="reg-label">{{regLabel}}</div>
-        <div class="reg-cont">{{regCont}}</div>
+    <div class="reg-text" :style="code?'borderBottom:0':''">
+        <div class="reg-line reg-label">{{regLabel}}</div>
+        <input type="text" class="reg-line reg-cont" :placeholder="regCont">
+        <div class="reg-line reg-code" v-if="code">获取验证码</div>
     </div>
 </template>
 <script>
     export default {
-        props:['reg-label','reg-cont']
+        props:['reg-label','reg-cont','code']
     }
 </script>
+<style lang="scss">
+    .reg-line{
+        display: inline-block;
+        vertical-align: middle;
+        padding-top: 10px;
+    }
+    .reg-text{
+        padding: 10px 20px;
+        display: flex;
+        border-bottom: 1px solid lightgray;
+    }
+    .reg-label{
+        flex:1
+    }
+    .reg-cont{
+        flex: 3;
+        text-indent: 2rem;
+    }
+    .reg-code{
+        right: 1rem;
+        position: absolute;
+        padding-left: .5rem ;
+        color: green;
+        border-left: 1px solid lightgray;
+        padding-bottom: 10px;
+    }
+</style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -5,8 +5,9 @@
             <reg-input regLabel="学校/小区" regCont="请输入地址"></reg-input>
             <reg-input regLabel="详细地址" regCont="楼宇门派/寝室号"></reg-input>
             <reg-input regLabel="手机号" regCont="请输入手机号"></reg-input>
-            <reg-input regLabel="验证码" regCont="6位验证码"></reg-input>
+            <reg-input regLabel="验证码" regCont="6位验证码" code="true"></reg-input>
         </form>
+        <button id="reg-btn">注册</button>
     </div>
 </template>
 <script>
@@ -20,13 +21,27 @@
 </script>
 <style lang="scss">
     #reg-container{
-        display: flex;
         flex-direction: column;
+        justify-content: center;
+        text-align: center;
         #reg-img{
-            flex:1
+            min-height: 8rem;
         }
         #reg-form{
-            flex: 4;
+            padding: 0 .7rem;
+            border-top: 1px solid lightgray;
+            border-bottom: 1px solid lightgray;
+        }
+        #reg-btn{
+            width: 80%;
+            height: 2rem;
+            background-color: rgb(74,171,75);
+            border-radius: 4px;
+            border:0;
+            color: white;
+            font-size: .7rem;
+            margin-top: 2.2rem;
+
         }
     }
 
